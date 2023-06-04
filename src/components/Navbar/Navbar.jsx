1b739604f7b3a2006d90cbe86e39c441a97393b4
24juice22 <mike.james.nunn@gmail.com>
2023-06-04 09:08:26 -0400

    update Navbar content and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,71 +9,77 @@ import './Navbar.scss';
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <div className='navbar__top'>
-        <Link to='/' className='navbar__brand'>
-          <h3 className='navbar__logo'>$Zer0</h3>
-          <span>BetZer0</span>
-        </Link>
-        <div className='navbar__buttons'>
-          <button className='btn'>Login</button>
-          <button className='btn'>JOIN NOW</button>
+      <div className='container'>
+        <div className='navbar__top'>
+          <Link to='/' className='navbar__brand'>
+            <h3 className='navbar__logo'>BZ</h3>
+            <span>BetZer0</span>
+          </Link>
+          <div className='navbar__buttons'>
+            <button className='btn navbar__btn--login'>Login</button>
+            <button className='btn btn--outline navbar__btn--join'>
+              JOIN NOW
+            </button>
+          </div>
         </div>
       </div>
       <div className='navbar__bottom'>
-        <ul className='list navbar__list'>
-          <li className='navbar__list-item'>
-            <Link className='navbar__link' to='/'>
-              <img
-                className='navbar__icon'
-                src={mlb}
-                alt='Major League Baseball Logo'
-              />
-              <span>MLB</span>
-            </Link>
-          </li>
-          <li className='navbar__list-item'>
-            <Link className='navbar__link' to='/'>
-              <img
-                className='navbar__icon'
-                src={nfl}
-                alt='National Football League Logo'
-              />
-              <span>NFL</span>
-            </Link>
-          </li>
-          <li className='navbar__list-item'>
-            <Link className='navbar__link' to='/'>
-              <img
-                className='navbar__icon'
-                src={nba}
-                alt='National Basketball Association Logo'
-              />
-              <span>NBA</span>
-            </Link>
-          </li>
-          <li className='navbar__list-item'>
-            <Link className='navbar__link' to='/'>
-              <img
-                className='navbar__icon'
-                src={nhl}
-                alt='National Hockey League Logo'
-              />
-              <span>NHL</span>
-            </Link>
-          </li>
-          <li className='navbar__list-item'>
-            <Link className='navbar__link' to='/'>
-              <i className='fa-solid fa-football navbar__icon'></i>
-              <span>NCAAF</span>
-            </Link>
-          </li>
-          <li className='navbar__list-item'>
-            <Link className='navbar__link' to='/'>
-              <i className='fa-solid fa-basketball navbar__icon'></i>
-              <span>NCAAB</span>
-            </Link>
-          </li>
-        </ul>
+        <div className='container--widest'>
+          <ul className='list navbar__list'>
+            <li className='navbar__list-item'>
+              <Link className='navbar__link' to='/'>
+                <img
+                  className='navbar__icon navbar__icon--baseball'
+                  src={mlb}
+                  alt='Major League Baseball Logo'
+                />
+                <span>MLB</span>
+              </Link>
+            </li>
+            <li className='navbar__list-item'>
+              <Link className='navbar__link' to='/'>
+                <img
+                  className='navbar__icon'
+                  src={nfl}
+                  alt='National Football League Logo'
+                />
+                <span>NFL</span>
+              </Link>
+            </li>
+            <li className='navbar__list-item'>
+              <Link className='navbar__link' to='/'>
+                <img
+                  className='navbar__icon'
+                  src={nba}
+                  alt='National Basketball Association Logo'
+                />
+                <span>NBA</span>
+              </Link>
+            </li>
+            <li className='navbar__list-item'>
+              <Link className='navbar__link' to='/'>
+                <img
+                  className='navbar__icon'
+                  src={nhl}
+                  alt='National Hockey League Logo'
+                />
+                <span>NHL</span>
+              </Link>
+            </li>
+            <li className='navbar__list-item'>
+              <Link className='navbar__link' to='/'>
+                <i className='fa-solid fa-football navbar__icon navbar__icon--ncaaf'></i>
+                <span>NCAAF</span>
+              </Link>
+            </li>
+            <li className='navbar__list-item'>
+              <Link className='navbar__link' to='/'>
+                <i className='fa-solid fa-basketball navbar__icon'></i>
+                <span>NCAAB</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
