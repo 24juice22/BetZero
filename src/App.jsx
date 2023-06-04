4ac123bd73cd0a1b1e589ce09b8289e74595bd4f
24juice22 <mike.james.nunn@gmail.com>
2023-06-04 08:26:48 -0400

    Update Structure of App, add BrowserRouter, begin global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import Betbar from './components/Betbar';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route index path='/' element={<Home />} />
+      </Routes>
+      <Betbar />
+    </>
+  );
 }
 
 export default App;
